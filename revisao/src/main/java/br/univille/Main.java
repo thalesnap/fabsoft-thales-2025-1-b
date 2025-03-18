e9cabc000deb687193cd76d5535af7724a7080fd
THALES NATAN ABREU PEDRI <thales.pedri@univille.br>
2025-03-18 20:21:20 -0300

    feat:Revisão Boa

--- revisao/src/main/java/br/univille/Main.java
+++ revisao/src/main/java/br/univille/Main.java
@@ -1,11 +1,21 @@
 package br.univille;
 
+import br.univille.entity.Cidade;
+import br.univille.entity.Cliente;
+
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
+        Cidade joinville = new Cidade();
+        joinville.setNome("Joinville");
+        joinville.setEstado("Santa Catarina");
+
         Cliente zezinho = new Cliente("Zezinho");
         zezinho.setNome("Zezinho da silva Sauro");
+        zezinho.setCidade(joinville);
+
+
         Cliente mariazinha = new Cliente();
         mariazinha.setNome("Mariazinha");
 
---
+++ revisao/src/main/java/br/univille/entity/Cliente.java
@@ -0,0 +1,69 @@
+package br.univille.entity;
+
+import java.util.ArrayList;
+
+public class Cliente {
+    //variável - atributo
+    private String nome;
+    private String endereco;
+    private long id;
+
+    private ArrayList listaPokemon;
+
+
+    public ArrayList getListaPokemon() {
+        return listaPokemon;
+    }
+    public void setListaPokemon(ArrayList listaPokemon) {
+        this.listaPokemon = listaPokemon;
+    }
+
+    //isso é a seta de associação no diagrama
+    private Cidade cidade;
+    
+    public Cidade getCidade() {
+        return cidade;
+    }
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    //construtor = mesmo nome da classe e sem um retorno
+    // func = inicializar atributos
+    // obrigar a passagem de valores
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    //polimorfismo - varias formas de existir a mesma coisa
+    public Cliente(){
+    
+    }
+    //metodo
+
+    //sobre escrita de metodo ( meu pai me deu pronto e eu reescrevi)
+    @Override
+    public String toString(){
+        return getNome();
+    }
+
+    //método
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    // encapsulamento = responder a implementacao dentro do objeto
+}
