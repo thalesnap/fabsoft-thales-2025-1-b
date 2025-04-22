1ddff41ebda10eb15b522efe52d4ca836a5e05a0
NICOLAS KAUÊ MARQUES BRIETZIG <nicolasbrietzig@univille.br>
2025-04-22 20:11:54 -0300

    feat:put delete

--- projfabsoft/src/main/java/br/univille/projfabsoft/controller/UsuarioController.java
+++ projfabsoft/src/main/java/br/univille/projfabsoft/controller/UsuarioController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,4 +41,40 @@ public class UsuarioController {
         }
         return ResponseEntity.badRequest().build();
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> 
+        putUsuario(@PathVariable long id, @RequestBody Usuario usuario){
+
+        if(id <= 0 || usuario == null){
+            return ResponseEntity.badRequest().build();
+        }
+        var usuarioAntigo = service.findById(id);
+        if(usuarioAntigo == null){
+            return ResponseEntity.notFound().build();
+        }
+        usuarioAntigo.setNome(usuario.getNome());
+        usuarioAntigo.setEmail(usuario.getEmail());
+        usuarioAntigo.setSenha(usuario.getSenha());
+        
+        service.save(usuarioAntigo);
+        return new ResponseEntity<Usuario>(usuarioAntigo, HttpStatus.OK);
+
+    }
+
+    public ResponseEntity<Usuario> deleteUsuario(@PathVariable long id){
+        if(id <= 0){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var usuarioExcluido = service.findById(id);
+        if(usuarioExcluido == null){
+            return ResponseEntity.notFound().build();
+        }
+        service.deleteById(id);
+        
+        return new ResponseEntity<Usuario>(usuarioExcluido, HttpStatus.OK)
+
+    }
+
+
 }
