43bdb1d3cc5dce27304aa1e6f2859bd8c41b65d5
thalesnap <thales.pedri@univille.br>
2025-06-10 00:54:37 +0000

    coisas

--- projfabsoft-frontend/src/app/app.routes.ts
+++ projfabsoft-frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { FormUsuarioComponent } from './form-usuario/form-usuario.component';
+
 
 export const routes: Routes = [
-    { path: 'usarios', component: UsuarioComponent}
+    { path: 'usarios', component: UsuarioComponent},
+    { path: 'usarios/novo', component: FormUsuarioComponent},
 ];
--- projfabsoft-frontend/src/app/usuario/usuario.component.ts
+++ projfabsoft-frontend/src/app/usuario/usuario.component.ts
@@ -3,10 +3,11 @@ import { Usuario } from '../model/usuario';
 import { UsuarioService } from '../service/usuario.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, RouterLink],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css',
   providers: [UsuarioService]
